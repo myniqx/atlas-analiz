9ef411a2c466b07726c8bed74440aac70793846d
Burak <burak_okur@hotmail.com>
2025-11-26 15:35:10 +0100

    Add Google Tag Manager script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,13 @@ export default function RootLayout({
   return (
     <html lang="tr" suppressHydrationWarning>
       <head>
+        <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KR5M7CRH');</script>
+<!-- End Google Tag Manager -->
         <meta name="google-site-verification" content="5J-ao5JNWSuoUDV3Rq4Wt1cM-0w4889pcYJVJidZ2Dk" />
         {/* <!-- Google tag (gtag.js) --> */}
         {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID_ATLAS_ANALIZ && (
@@ -49,6 +56,10 @@ export default function RootLayout({
         )}
       </head>
       <body className={inter.className}>
+        <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KR5M7CRH"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
