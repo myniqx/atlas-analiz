39805537bc9220809fe9c46bd6239bc9c3036780
Burak <burak_okur@hotmail.com>
2025-11-26 15:41:50 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,11 +26,11 @@ export default function RootLayout({
   return (
     <html lang="tr" suppressHydrationWarning>
       <head>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+<Script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KR5M7CRH');</script>
+})(window,document,'script','dataLayer','GTM-KR5M7CRH');</Script>
         <meta name="google-site-verification" content="5J-ao5JNWSuoUDV3Rq4Wt1cM-0w4889pcYJVJidZ2Dk" />
         {/* <!-- Google tag (gtag.js) --> */}
         {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID_ATLAS_ANALIZ && (
